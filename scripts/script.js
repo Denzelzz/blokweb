e525d6bdc98ad0be3cf87116c7bb7eac03156fbe
Denzelzz <70603396+Denzelzz@users.noreply.github.com>
2020-11-01 16:36:50 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,10 @@
-// JavaScript Document
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// button in de nav die het menu moet gaan openen en sluiten
+var deButton = document.querySelector("nav ul:first-of-type li:first-of-type");
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// het hamburgermenu
+var menu = document.querySelector("nav ul:nth-of-type(2)");
+
+deButton.addEventListener("click", function () {
+  //via deze functie moet de class "toonMenu" het menu tevoorschijn halen.
+   menu.classList.toggle("toonMenu");
+});
